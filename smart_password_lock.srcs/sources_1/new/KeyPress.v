d5a75280b2581cce68e139be3dd9971f95cfdf3c
Flight_leaf <718856370@qq.com>
2023-10-19 23:05:25 +0800

    KeyValue.v

--- smart_password_lock.srcs/sources_1/new/KeyPress.v
+++ smart_password_lock.srcs/sources_1/new/KeyPress.v
@@ -7,18 +7,18 @@
 //时间:2021-4-24
 //***************************************************************************
 module KeyPress(
-	CLK,
-	nRST,
-	KEY_IN,
-	KEY_FLAG,
-	KEY_STATE
+	clk,
+	reset,
+	key_in,
+	key_flag,
+	key_state
 );
-	input CLK;
-	input nRST;
-	input KEY_IN;
+	input clk;
+	input reset;
+	input key_in;
 	
-	output reg KEY_FLAG;			//按键按下标志位 高电平为按下
-	output reg KEY_STATE;
+	output reg key_flag;			//按键按下标志位 高电平为按下
+	output reg key_state;
 	
 	reg key_a, key_b;
 	reg en_cnt, cnt_full;
@@ -35,23 +35,23 @@ module KeyPress(
 		Filter_Down2Up	=	4'b1000;
 		
 	//======判断按键输入信号跳变沿========//
-	always @(posedge CLK or negedge nRST)
-		if(!nRST)
+	always @(posedge clk or negedge reset)
+		if(!reset)
 			begin
 				key_a <= 1'b0;
 				key_b <= 1'b0;
 			end
 		else
 			begin
-				key_a <= KEY_IN;
+				key_a <= key_in;
 				key_b <= key_a;
 			end
 	assign flag_H2L = key_b && (!key_a);
 	assign flag_L2H = (!key_b) && key_a;
 	
 	//============计数使能模块==========//
-	always @(posedge CLK or negedge nRST)
-		if(!nRST)
+	always @(posedge clk or negedge reset)
+		if(!reset)
 			cnt <= 1'b0;
 		else if(en_cnt)
 			cnt <= cnt + 1'b1;
@@ -59,8 +59,8 @@ module KeyPress(
 			cnt <= 1'b0;
 			
 	//=============计数模块=============//
-	always @(posedge CLK or negedge nRST)
-		if(!nRST)
+	always @(posedge clk or negedge reset)
+		if(!reset)
 			cnt_full <= 1'b0;
 		else if(cnt == 20'd999_999)
 			cnt_full <= 1'b1;
@@ -68,19 +68,19 @@ module KeyPress(
 			cnt_full <= 1'b0;
 	
 	//=============有限状态机============//
-	always @(posedge CLK or negedge nRST)
-		if(!nRST)
+	always @(posedge clk or negedge reset)
+		if(!reset)
 			begin
 				en_cnt <= 1'b0;
 				state <= Key_up;
-				KEY_FLAG <= 1'b0;
-				KEY_STATE <= 1'b1;
+				key_flag <= 1'b0;
+				key_state <= 1'b1;
 			end
 		else
 			case(state)
 				//保持没按
 				Key_up: begin 
-					KEY_FLAG <= 1'b0;
+					key_flag <= 1'b0;
 					if(flag_H2L) begin
 						state <= Filter_Up2Down;
 						en_cnt <= 1'b1;
@@ -93,8 +93,8 @@ module KeyPress(
 					if(cnt_full) begin
 						en_cnt <= 1'b0;
 						state <= Key_down;
-						KEY_STATE <= 1'b0;
-						KEY_FLAG <= 1'b1;
+						key_state <= 1'b0;
+						key_flag <= 1'b1;
 					end
 					else if(flag_L2H) begin
 						en_cnt <= 1'b0;
@@ -105,7 +105,7 @@ module KeyPress(
 				end
 				//保持按下状态
 				Key_down: begin
-					KEY_FLAG <= 1'b0;
+					key_flag <= 1'b0;
 					if(flag_L2H) begin
 						state <= Filter_Down2Up;
 						en_cnt <= 1'b1;
@@ -118,8 +118,8 @@ module KeyPress(
 					if(cnt_full) begin
 						en_cnt <= 1'b0;
 						state <= Key_up;
-						KEY_FLAG <= 1'b1;
-						KEY_STATE <= 1'b1;
+						key_flag <= 1'b1;
+						key_state <= 1'b1;
 					end
 					else if(flag_H2L) begin
 						en_cnt <= 1'b0;
@@ -132,8 +132,8 @@ module KeyPress(
 				default: begin
 					en_cnt <= 1'b0;
 					state <= Key_up;
-					KEY_FLAG <= 1'b0;
-					KEY_STATE <= 1'b1;
+					key_flag <= 1'b0;
+					key_state <= 1'b1;
 				end
 			endcase	
 endmodule
--- smart_password_lock.srcs/sources_1/new/KeyValue.v
+++ smart_password_lock.srcs/sources_1/new/KeyValue.v
@@ -7,19 +7,20 @@
 //时间:2021-4-24
 //***************************************************************************
 module KeyValue(
-	CLK_,
-	nRST,
-	KEY_ROW,
-	KEY_COL,
-	KEY_Value,
-	Value_en
+	clk,
+	reset,
+	key_row,
+	key_col,
+	key_value,
+	value_en
 );
-	input CLK_;
-	input nRST;
-	input [3:0]KEY_COL;				//列
-	output reg Value_en;
-	output reg [3:0]KEY_ROW;		//行
-	output reg [3:0]KEY_Value;		//矩阵键盘按下的值
+	input clk;
+	input reset;
+	input [3:0] key_col;				//列
+
+	output reg value_en;
+	output reg [3:0]key_row;		//行
+	output reg [3:0]key_value;		//矩阵键盘按下的值
 	
 	wire [3:0]key_flag;				//按键标志位
 	wire [3:0]key_state;
@@ -28,108 +29,73 @@ module KeyValue(
 	reg row_flag;						//标识已定位到行
 	reg [1:0]rowIndex;				//行索引
 	reg [1:0]colIndex;				//列索引
-    reg reset = 1;
-
-    frequency_division frequency_division(CLK_,reset,clk1k,clk2k,clk3k,CLK);
 	
-	localparam
+	parameter
 		NO_KEY		=	5'b00001,
 		ROW_ONE		=	5'b00010,
 		ROW_TWO		=	5'b00100,
 		ROW_THREE	=	5'b01000,
 		ROW_FOUR	=	5'b10000;
 		
-	KeyPress u0(
-		.CLK(CLK),
-		.nRST(nRST),
-		.KEY_IN(KEY_COL[0]),
-		.KEY_FLAG(key_flag[0]),
-		.KEY_STATE(key_state[0])
-	);
+	KeyPress u0(clk,reset,key_col[0],key_flag[0],key_state[0]);
+	KeyPress u1(clk,reset,key_col[1],key_flag[1],key_state[1]);
+	KeyPress u2(clk,reset,key_col[2],key_flag[2],key_state[2]);
+	KeyPress u3(clk,reset,key_col[3],key_flag[3],key_state[3]);
 	
-	KeyPress u1(
-		.CLK(CLK),
-		.nRST(nRST),
-		.KEY_IN(KEY_COL[1]),
-		.KEY_FLAG(key_flag[1]),
-		.KEY_STATE(key_state[1])
-	);
-	
-	KeyPress u2(
-		.CLK(CLK),
-		.nRST(nRST),
-		.KEY_IN(KEY_COL[2]),
-		.KEY_FLAG(key_flag[2]),
-		.KEY_STATE(key_state[2])
-	);
-	
-	KeyPress u3(
-		.CLK(CLK),
-		.nRST(nRST),
-		.KEY_IN(KEY_COL[3]),
-		.KEY_FLAG(key_flag[3]),
-		.KEY_STATE(key_state[3])
-	);
-
 	//==========通过状态机判断行===========//
-	always @(posedge CLK or negedge nRST)
-		if(!nRST)
-			begin
+	always @(posedge clk or posedge reset) begin
+		if(reset) begin
 				state <= NO_KEY;
 				row_flag <= 1'b0;
-				KEY_ROW <= 4'b0000;
-			end
-		else
+				key_row <= 4'b0000;
+		end	else begin
 			case(state)
 				NO_KEY: begin
 					row_flag <= 1'b0;
-					KEY_ROW <= 4'b0000;	
+					key_row <= 4'b0000;	
 					if(key_flag != 4'b0000) begin
 						state <= ROW_ONE;
-						KEY_ROW <= 4'b1110;
-					end
-					else
+						key_row <= 4'b1110;
+					end	else begin
 						state <= NO_KEY;
+					end
 				end
 				
 				ROW_ONE: begin
-					if(KEY_COL != 4'b1111) begin
+					if(key_col != 4'b1111) begin
 						state <= NO_KEY;
 						rowIndex <= 4'd0;
 						row_flag <= 1'b1;
-					end
-					else begin
+					end	else begin
 						state <= ROW_TWO;
-						KEY_ROW <= 4'b1101;
+						key_row <= 4'b1101;
 					end						
 				end
 				
 				ROW_TWO: begin
-					if(KEY_COL != 4'b1111) begin
+					if(key_col != 4'b1111) begin
 						state <= NO_KEY;
 						rowIndex <= 4'd1;
 						row_flag <= 1'b1;
-					end
-					else begin
+					end	else begin
 						state <= ROW_THREE;
-						KEY_ROW <= 4'b1011;
+						key_row <= 4'b1011;
 					end						
 				end
 				
 				ROW_THREE: begin
-					if(KEY_COL != 4'b1111) begin
+					if(key_col != 4'b1111) begin
 						state <= NO_KEY;
 						rowIndex <= 4'd2;
 						row_flag <= 1'b1;
-					end
-					else begin
+					end	else begin
 						state <= ROW_FOUR;
-						KEY_ROW <= 4'b0111;
+						key_row <= 4'b0111;
 					end						
 				end
 				
 				ROW_FOUR: begin
-					if(KEY_COL != 4'b1111) begin
+					if(key_col != 4'b1111) begin
 						rowIndex <= 4'd3;
 						row_flag <= 1'b1;
 					end
@@ -140,31 +106,34 @@ module KeyValue(
                     state <= NO_KEY;
                 end
 			endcase
-	
+		end	
+	end
 	//===========判断按键所在列=============//
-	always @(posedge CLK or negedge nRST)
-		if(!nRST)
+	always @(posedge clk or posedge reset) begin
+		if(reset) begin
 			colIndex <= 2'd0;
-		else if(key_state != 4'b1111)
+		end	else if(key_state != 4'b1111) begin
 			case(key_state)
 				4'b1110: colIndex <= 2'd0;
 				4'b1101: colIndex <= 2'd1;
 				4'b1011: colIndex <= 2'd2;
 				4'b0111: colIndex <= 2'd3;
 			endcase
+		end	
+	end
+		
 	
 	//===========通过行列计算键值==========//
-	always @(posedge CLK or negedge nRST)
-		if(!nRST)
-			Value_en <= 1'b0;
-		else if(row_flag)
-			begin
-				Value_en <= 1'b1;
-				KEY_Value <= 4*rowIndex + colIndex;
-			end
-		else
-			Value_en <= 1'b0;
-			
+	always @(posedge clk or posedge reset) begin
+		if(reset) begin
+			value_en <= 1'b0;
+		end	else if(row_flag) begin
+			value_en <= 1'b1;
+			key_value <= 4*rowIndex + colIndex;
+		end else begin
+			value_en <= 1'b0;
+		end
+	end
 endmodule
 
 
